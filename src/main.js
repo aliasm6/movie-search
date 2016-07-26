f6cec735d008ef04e7cceeb3222991327bf615f2
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-07-26 17:43:51 -0600

    finished backend

--- src/main.js
+++ src/main.js
@@ -15,23 +15,25 @@ $(document).ready(function() {
         method: 'GET',
         url: 'http://www.omdbapi.com/?t=' + title
     }).done(function (data){
-
+      var genre = data.Genre;
+      console.log(genre);
+      var genres = genre.split(', ');
       var poster = data.Poster;
       var title = data.Title
 
       $('.row').append('<div class="col-md-4"></div>');
 
-      $('.col-md-4:nth-of-type(' + buttonCounter + ')').append('<h1>' + title + '</h1>');
+      $('.col-md-4:nth-of-type(' + buttonCounter + ')').append('<h2 class="' + genres.join(' ') + '">' + title + '</h2>');
 
-      $('.col-md-4:nth-of-type(' + buttonCounter + ')').append('<img src="' + data.Poster + '">');
+      $('.col-md-4:nth-of-type(' + buttonCounter + ')').append('<img src="' + data.Poster + '" class="' + genres.join(' ') + '">');
 
-      var genre = data.Genre;
-      console.log(genre);
-      var genres = genre.split(', ');
+
 
       var counter = 1;
 
       for (var i = 0; i < genres.length; i++) {
+
+        console.log(genres[i]);
 
         if (!genreArray.includes(genres[i]))
         {
@@ -43,6 +45,27 @@ $(document).ready(function() {
           counter++;
         }
       }
+
+      $('#searchInfo').val('');
+
+
+
+      $('select').change(function () {
+      $('option:selected').each(function () {
+
+        var cLass = $(this).text();
+        console.log(cLass);
+
+        $('img').css('display', 'block');
+        $('h2').css('display', 'block');
+
+        if ($('img').hasClass(cLass))
+        {
+          $('img:not(.' + cLass + ')').css('display', 'none');
+          $('h2:not(.' + cLass + ')').css('display', 'none');
+        }
+      });
+    });
 
 
       // console.log(genreArray);
